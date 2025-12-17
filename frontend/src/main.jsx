a4c142bfcfcd8863b058b2375b3af84e41c0a22e
Mohith Kuruba <mohithkuruba83@gmail.com>
2025-12-17 14:03:22 +0530

    Fix white screen by wrapping app with AuthProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "./index.css";
-
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
